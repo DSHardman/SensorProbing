88a65d8402d6eee1489fe7a846a2923bbd652d31
DSHardman <dshardman@outlook.com>
2021-11-20 11:42:35 +0000

    set alignment start to zero

--- AlignSensor.py
+++ AlignSensor.py
@@ -4,7 +4,7 @@ import time
 import numpy as np
 
 zeropose = [-0.0718167, -0.443351, 0.0075692, -1.45804, 2.77907, 0.00753163]
-startingpose = np.add(zeropose, [x, y, 0.01, 0, 0, 0])
+startingpose = np.add(zeropose, [0, 0, 0.01, 0, 0, 0])
 
 #  Connect to UR5
 urnie = kgr.kg_robot(port=30010, db_host="169.254.150.50")
