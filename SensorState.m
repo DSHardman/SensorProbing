39d234cc131082a9cdab6bf9e4ebf3f22f7d9cc6
David <dshardman@outlook.com>
2021-12-02 11:47:09 +0000

    animated line press response

--- SensorState.m
+++ SensorState.m
@@ -20,6 +20,26 @@ classdef SensorState < handle
                 error('Check Number of Inputs');
             end
         end
+        
+         function animateline(obj)
+             
+             obj.line.plotresponse(1);
+             for i = 2:obj.line.n
+                obj.line.plotresponse(i);
+                
+                subplot(1,2,1);
+                children = get(gca, 'Children');
+                delete(children(end-8:end));
+                
+                subplot(1,2,2);
+                children = get(gca, 'Children');
+                delete(children(end-7:end));
+                legend('Orientation', 'Horizontal', 'Location', 's');
+                legend boxoff
+                
+                pause(0.1);
+             end
+         end
     end
 end
 
--- SingleTest.m
+++ SingleTest.m
@@ -62,14 +62,48 @@ classdef SingleTest < handle
         
         % plot raw sensor responses
         function plotresponse(obj, iteration, sensor)
+           
+            set(gcf, 'Position', 1000*[0.0130    0.3850    1.5032    0.4200]);
+            subplot(1,2,2)
             if nargin == 3
                 plot(obj.rawresponses(iteration,:,sensor));
             else
+                colors = [0 0.447 0.741;...
+                            0.85 0.325 0.98;...
+                            0.929 0.694 0.125;...
+                            0.494 0.184 0.556;...
+                            0.466 0.674 0.188;...
+                            0.301 0.745 0.933;...
+                            0.635 0.078 0.184;
+                            0 0 0];
                 for i = 1:8
-                    plot(obj.rawresponses(iteration,:,i));
+                    plot(obj.rawresponses(iteration,:,i), 'LineWidth', 2,...
+                        'Color', colors(i,:), 'DisplayName', string(i-1));
                     hold on
                 end
             end
+            ylim([min(min(min(obj.rawresponses)))-0.5 max(max(max(obj.rawresponses)))]);
+            xlim([0 308])
+            set(gca, 'LineWidth', 2, 'FontSize', 15, 'XTickLabel', []);
+            box off
+            ylabel('Sensor Response (V)');
+            titlestring = sprintf('x = %.3fmm, y = %.3fmm',...
+                obj.positions(iteration,1), obj.positions(iteration,2));
+            title(titlestring);
+            
+            
+            subplot(1,2,1);
+            for i = 1:4
+                line([-11.5 46], [(i-1)*11.5 (i-1)*11.5]', 'Color', colors((9-i),:),...
+                    'LineWidth', 2);
+                line([(i-1)*11.5 (i-1)*11.5]', [-11.5 46], 'Color', colors((5-i),:),...
+                    'LineWidth', 2);
+            end
+            set(gca, 'Visible', 'off')
+            ylim([-16.5 51]); xlim([-16.5 51]); axis square
+            hold on
+            scatter(obj.positions(iteration,1), obj.positions(iteration,2),...
+                150, 'r', 'filled');
         end
         
     end
