7d8a025b6568343110f189d5af43fcf48eb0622c
David <dshardman@outlook.com>
2022-01-06 18:52:43 +0000

    created and saved transfer result objects

--- CreateTransferObject.m
+++ CreateTransferObject.m
@@ -1,44 +1,44 @@
-xythree = Attempt(H12_grid(:,:,1), 'h12_grid_3');
-xyfive = Attempt(H12_grid(:,:,2), 'h12_grid_5');
-xyseven = Attempt(H12_grid(:,:,3), 'h12_grid_7');
-dpthree = Attempt(h12_grid(:,:,1), 'h12_grid_3');
-dpfive = Attempt(h12_grid(:,:,2), 'h12_grid_5');
-dpseven = Attempt(h12_grid(:,:,3), 'h12_grid_7');
+xythree = Attempt(H25_grid(:,:,1), 'h25_grid_3');
+xyfive = Attempt(H25_grid(:,:,2), 'h25_grid_5');
+xyseven = Attempt(H25_grid(:,:,3), 'h25_grid_7');
+dpthree = Attempt(h25_grid(:,:,1), 'h25_grid_3');
+dpfive = Attempt(h25_grid(:,:,2), 'h25_grid_5');
+dpseven = Attempt(h25_grid(:,:,3), 'h25_grid_7');
 grid = Approach(xythree, xyfive, xyseven, dpthree, dpfive, dpseven);
 
-xythree = Attempt(H12_random(:,:,1), 'h12_random_3');
-xyfive = Attempt(H12_random(:,:,2), 'h12_random_5');
-xyseven = Attempt(H12_random(:,:,3), 'h12_random_7');
-dpthree = Attempt(h12_random(:,:,1), 'h12_random_3');
-dpfive = Attempt(h12_random(:,:,2), 'h12_random_5');
-dpseven = Attempt(h12_random(:,:,3), 'h12_random_7');
+xythree = Attempt(H25_random(:,:,1), 'h25_random_3');
+xyfive = Attempt(H25_random(:,:,2), 'h25_random_5');
+xyseven = Attempt(H25_random(:,:,3), 'h25_random_7');
+dpthree = Attempt(h25_random(:,:,1), 'h25_random_3');
+dpfive = Attempt(h25_random(:,:,2), 'h25_random_5');
+dpseven = Attempt(h25_random(:,:,3), 'h25_random_7');
 random = Approach(xythree, xyfive, xyseven, dpthree, dpfive, dpseven);
 
-xythree = Attempt(H12_weighted(:,:,1), 'h12_weighted_3');
-xyfive = Attempt(H12_weighted(:,:,2), 'h12_weighted_5');
-xyseven = Attempt(H12_weighted(:,:,3), 'h12_weighted_7');
-dpthree = Attempt(h12_weighted(:,:,1), 'h12_weighted_3');
-dpfive = Attempt(h12_weighted(:,:,2), 'h12_weighted_5');
-dpseven = Attempt(h12_weighted(:,:,3), 'h12_weighted_7');
+xythree = Attempt(H25_weighted(:,:,1), 'h25_weighted_3');
+xyfive = Attempt(H25_weighted(:,:,2), 'h25_weighted_5');
+xyseven = Attempt(H25_weighted(:,:,3), 'h25_weighted_7');
+dpthree = Attempt(h25_weighted(:,:,1), 'h25_weighted_3');
+dpfive = Attempt(h25_weighted(:,:,2), 'h25_weighted_5');
+dpseven = Attempt(h25_weighted(:,:,3), 'h25_weighted_7');
 weighted = Approach(xythree, xyfive, xyseven, dpthree, dpfive, dpseven);
 
-xythree = Attempt(H12_2d(:,:,1), 'h12_2d_3');
-xyfive = Attempt(H12_2d(:,:,2), 'h12_2d_5');
-xyseven = Attempt(H12_2d(:,:,3), 'h12_2d_7');
-dpthree = Attempt(h12_2d(:,:,1), 'h12_2d_3');
-dpfive = Attempt(h12_2d(:,:,2), 'h12_2d_5');
-dpseven = Attempt(h12_2d(:,:,3), 'h12_2d_7');
+xythree = Attempt(H25_2d(:,:,1), 'h25_2d_3');
+xyfive = Attempt(H25_2d(:,:,2), 'h25_2d_5');
+xyseven = Attempt(H25_2d(:,:,3), 'h25_2d_7');
+dpthree = Attempt(h25_2d(:,:,1), 'h25_2d_3');
+dpfive = Attempt(h25_2d(:,:,2), 'h25_2d_5');
+dpseven = Attempt(h25_2d(:,:,3), 'h25_2d_7');
 twod = Approach(xythree, xyfive, xyseven, dpthree, dpfive, dpseven);
 
-xythree = Attempt(H12_invert(:,:,1), 'h12_invert_3');
-xyfive = Attempt(H12_invert(:,:,2), 'h12_invert_5');
-xyseven = Attempt(H12_invert(:,:,3), 'h12_invert_7');
-dpthree = Attempt(h12_invert(:,:,1), 'h12_invert_3');
-dpfive = Attempt(h12_invert(:,:,2), 'h12_invert_5');
-dpseven = Attempt(h12_invert(:,:,3), 'h12_invert_7');
+xythree = Attempt(H25_invert(:,:,1), 'h25_invert_3');
+xyfive = Attempt(H25_invert(:,:,2), 'h25_invert_5');
+xyseven = Attempt(H25_invert(:,:,3), 'h25_invert_7');
+dpthree = Attempt(h25_invert(:,:,1), 'h25_invert_3');
+dpfive = Attempt(h25_invert(:,:,2), 'h25_invert_5');
+dpseven = Attempt(h25_invert(:,:,3), 'h25_invert_7');
 invert = Approach(xythree, xyfive, xyseven, dpthree, dpfive, dpseven);
 
-basecase = SingleCase(2.64, 98.82, 'H12NET');
-zerocase = SingleCase(3.43, 34.40, 'H12_ZERO');
+basecase = SingleCase(2.64, 98.82, 'H25NET');
+zerocase = SingleCase(3.43, 34.40, 'H25_ZERO');
 
-H12 = Transfer(basecase, zerocase, grid, random, weighted, twod, invert);
+H25 = Transfer(basecase, zerocase, grid, random, weighted, twod, invert);
