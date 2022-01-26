5c6ff5f00aca3b3d5d61036085c00a704080a166
David <dshardman@outlook.com>
2022-01-26 08:49:41 +0000

    labels to setup figure

--- PlotResponses.m
+++ PlotResponses.m
@@ -43,14 +43,31 @@ xlabel('Time (s)');
 ylabel('Sensor Response (V)');
 set(gcf, 'Position', [489.0000  586.6000  582.4000  272.4000]);
 
-figure()
-for i = 1:8
-    plot(0.001:0.001:30, response2_5(1:30000,i), 'LineWidth', 2, 'Color', colors(i,:));
-    hold on
+
+%% Animate
+
+%figure()
+tic
+for i = 1:100:30000
+    for j = 1:8
+        plot(0.001:0.001:0.001*i, response4(1:i,j), 'LineWidth', 2, 'Color', colors(j,:));
+        hold on
+    end
+    if i == 1
+        set(gca, 'LineWidth', 2, 'FontSize', 15);
+        box off
+        ylim([0 5.25]);
+        xlim([0 30])
+        xlabel('Time (s)');
+        ylabel('Sensor Response (V)');
+        set(gcf, 'Position', [489.0000  554.6000  660.0000  304.4000],...
+            'Color', 'w');
+    else
+        children = get(gca, 'children');
+        delete(children(9:end));
+        legend({'1';'2';'3';'4';'5';'6';'7';'8'},...
+            'Orientation', 'horizontal', 'Location', 's');
+        legend boxoff
+    end
+    pause(0.02);
 end
-set(gca, 'LineWidth', 2, 'FontSize', 15);
-box off
-ylim([0 5.25]);
-xlabel('Time (s)');
-ylabel('Sensor Response (V)');
-set(gcf, 'Position', [489.0000  586.6000  582.4000  272.4000]);
